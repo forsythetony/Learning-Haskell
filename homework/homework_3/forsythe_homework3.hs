e350981c0814777e6c23ce571a2d603d022dd081
Tony Forsythe <forsythetony@gmail.com>
2017-11-08 14:18:35 -0600

    Adds implementation for problem 2

--- homework/homework_3/forsythe_homework3.hs
+++ homework/homework_3/forsythe_homework3.hs
@@ -15,8 +15,19 @@ prob1 x   = prob1' (words x)
                 typeValue "*" = Mul
                 typeValue a = Val (read a)
 
-prob2    :: a
-prob2    = undefined
+prob2    :: PExp -> Int
+prob2 ops = prob2' ops []
+
+
+prob2' :: PExp -> [Int] -> Int
+prob2' ((Val i):xs) ans         = prob2' xs (i:ans)
+prob2' (Plus:xs) (r:l:ins)      = prob2' xs ((l + r):ins)
+prob2' (Minus:xs) (r:l:ins)     = prob2' xs ((l - r):ins)
+prob2' (Mul:xs) (r:l:ins)       = prob2' xs ((l * r):ins)
+prob2' (IntDiv:xs) (r:l:ins)    = prob2' xs ((l `div` r):ins)
+prob2' (IntDiv:xs) (0:l:ins)    = error "Division by zero!"
+prob2' [] [i]                   = i
+prob2' _  _                     = error "Some undefined error in problem 2"
 
 prob3    :: a
 prob3    = undefined
