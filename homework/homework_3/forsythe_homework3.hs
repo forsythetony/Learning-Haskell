0215ce9b2ea734767f06c42df1b102306fb0b6de
Tony Forsythe <forsythetony@gmail.com>
2017-11-10 16:30:50 -0600

    Adds description to prob1

--- homework/homework_3/forsythe_homework3.hs
+++ homework/homework_3/forsythe_homework3.hs
@@ -6,6 +6,29 @@ import Test.Hspec
 import RPNAST
 import Control.Exception
 
+
+-- Function prob1
+-- @author
+--  Anthony Forsythe
+--
+-- @type
+--  (String) -> PExp
+--
+-- @param
+--    This function takes the following as inputs:
+--      - A string containing operators, integer consnants, and whitespace.
+--        ex: "200 + - / *"
+--
+-- @output
+--    A PExp which will be a list of the operators (Op) in the string.
+--    ex: [Val 200, Plus, Minus, IntDiv, Mul]
+--
+-- @description:
+--    This function will first use the built in `words` function to convert the
+--    string into a list of words that have been delimited by whitespace. Next
+--    it will recursively operate of the list and built a PExp by using a
+--    sub-function to convert the current element to an Op type before adding it
+--    to the list (PExp) that is eventually returned.
 prob1   :: String -> PExp
 prob1 x   = prob1' (words x)
   where prob1' []     = []
@@ -52,27 +75,34 @@ prob4    = undefined
 
 -- Write your Hspec Tests below
 
+test_probs = do
+  test_prob1
+  test_prob2
+  test_prob3
+
+
 test_prob1 = hspec $ do
   describe "Prob1 from HW 3" $ do
+
     context "Basic expression \"3 2 +\"" $ do
       it "should return [Val 3, Val 2, Plus]" $ do
         prob1 "3 2 +" `shouldBe` [Val 3, Val 2, Plus]
 
-  describe "Pob1 from HW 3" $ do
     context "Slightly more complicated expression of \"200 + - * /\"" $ do
       it "should return [Val 200, Plus, Minus, Mul, IntDiv]" $ do
         prob1 "200 + - * /" `shouldBe` [Val 200, Plus, Minus, Mul, IntDiv]
 
 test_prob2 = hspec $ do
   describe "Prob2 from HW 3" $ do
+
     context "Evaluation for basic integer division -> [Val 4, Val 2, IntDiv]" $ do
       it "should return 2" $ do
         prob2 [Val 4, Val 2, IntDiv] `shouldBe` 2
 
-  describe "Prob2 from HW 3" $ do
     context "Evaluation for bad input [Mul]" $ do
       it "should throw an error" $ do
         evaluate (prob2 [Mul]) `shouldThrow` anyException
+
 
 test_prob3 = hspec $ do
   describe "Prob3 from HW 3" $ do
@@ -80,7 +110,10 @@ test_prob3 = hspec $ do
       it "Should return failure" $ do
         prob3 [Val 5, Val 0, IntDiv] `shouldBe` Failure DivByZero
 
-  describe "Prob3 from HW 3" $ do
     context "Invalid input -> [IntDiv, Plus, Val 0]" $ do
       it "Should return failure" $ do
-        prob3 [IntDiv, Plus, Val 0] `shouldBe` Failure BadSyntax 
+        prob3 [IntDiv, Plus, Val 0] `shouldBe` Failure BadSyntax
+
+    context "Valid input -> [Val 5, Val 1, Val 1, Plus, Mul]" $ do
+      it "Should return `Success 10`" $ do
+        prob3 [Val 5, Val 1, Val 1, Plus, Mul] `shouldBe` Success 10
