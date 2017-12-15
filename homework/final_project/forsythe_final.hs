5f415e9c38caaae9d5d81abacdb2cfd63a1ad2ed
Tony Forsythe <forsythetony@gmail.com>
2017-12-15 00:21:26 -0600

    Adds a description for problem 1 'eval' and the skeleton of a description for prob2

--- homework/final_project/forsythe_final.hs
+++ homework/final_project/forsythe_final.hs
@@ -39,10 +39,17 @@ exp3     = parseExp "var a = 2, b = 7; (var m = 5 * a, n = b - 1; a * n + b / m)
 exp4     = parseExp "var a = 2, b = 7; (var m = 5 * a, n = m - 1; a * n + b / m) + a"
 -- N.b.,                                                  ^^^ is a free occurence of m (by Rule 2)
 
------------------
--- The evaluation function for the recursive function language.
------------------
-
+-- Function: eval
+--
+-- @original description
+--  The evaluation function for the recursive function language.
+--
+-- @type
+--  Exp -> Env -> Value
+--
+-- @description:
+--    Evaluates a given expression with an environment and returns the computed value
+--
 eval :: Exp -> Env -> Value
 eval (Literal v) env                = v
 eval (Unary op a) env               = unary  op (eval a env)
@@ -84,6 +91,14 @@ when doing the Declare case.
 freeChecker seen free ((v,e):rest)  = freeChecker (v:seen) (freeByRule1 seen e ++ free) rest
 freeChecker seen free []            = free
 
+-- Function: freeByRule1
+--
+-- @type
+--  [String] -> Exp -> [String]
+--
+-- @description:
+--
+--
 freeByRule1 :: [String] -> Exp -> [String]
 freeByRule1 seen (Literal _)            = []
 freeByRule1 seen (Unary _ e)            = freeByRule1 seen e
@@ -93,26 +108,21 @@ freeByRule1 seen (Variable x)           = if (elem x seen) then [] else [x]
 freeByRule1 seen (Declare ds body)     = let r1 = freeChecker seen [] ds
                                             in  r1 ++ freeByRule1 (vars ++ seen) body
                                             where vars = map fst ds
-                                            
+
 freeByRule1 seen (RecDeclare x e body)  = freeByRule1 (x:seen) e ++ freeByRule1 (x:seen) body
 freeByRule1 seen (Function x e)         = freeByRule1 (x:seen) e
 freeByRule1 seen (Call e1 e2)           = freeByRule1 seen e1 ++ freeByRule1 seen e2
 
-{--
-Literal Value
-  | Unary UnaryOp Exp
-  | Binary BinaryOp Exp Exp
-  | If Exp Exp Exp
-  | Variable String
-  | Declare [(String, Exp)] Exp
-  | RecDeclare String Exp Exp
-  | Function String Exp
-  | Call Exp Exp
---}
 
+-- Function: freeByRule2
+--
+-- @type
+--  [String] -> Exp -> [String]
+--
+-- @description:
+--
+--
 freeByRule2 :: [String] -> Exp -> [String]
-
-
 freeByRule2 seen (Literal _)            = []
 freeByRule2 seen (Unary _ e)            = freeByRule2 seen e
 freeByRule2 seen (Binary _ e1 e2)       = freeByRule2 seen e1 ++ freeByRule2 seen e2
@@ -157,6 +167,7 @@ freeByRule2 seen (Call e1 e2)           = freeByRule2 seen e1 ++ freeByRule2 see
 
 test_probs = do
   test_prob1
+  test_prob2
 
 test_prob1 = hspec $ do
   describe "Problem 1 from Final Project" $ do
@@ -164,3 +175,22 @@ test_prob1 = hspec $ do
     context "Basic" $ do
       it "should return IntV 120" $ do
         execute facrec `shouldBe` IntV 120
+
+test_prob2 = hspec $ do
+  describe "Problem 2 from Final Project (`freeByRule` problems)" $ do
+
+    context "freeByRule1 test with exp3" $ do
+      it "Should return empty list" $ do
+        freeByRule1 [] exp3 `shouldBe` []
+
+    context "freeByRule1 test with exp4" $ do
+      it "Should return empty list" $ do
+        freeByRule1 [] exp4 `shouldBe` []
+
+    context "freeByRule2 test with exp3" $ do
+      it "Should return empty list" $ do
+        freeByRule2 [] exp3 `shouldBe` []
+
+    context "freeByRule2 test with exp4" $ do
+      it "Should return a list containing [\"m\"]" $ do
+        freeByRule2 [] exp4 `shouldBe` ["m"]
